5f713f0c192572fad3bf1bc6832d65a1011ede20
Mert Ozan Kahraman <mozankahraman69@gmail.com>
2023-04-04 20:42:01 +0300

    SignIn/Up fragment UI added.

--- app/src/main/java/com/meetozan/e_commerce/ui/sign_up/SignUpFragment.kt
+++ app/src/main/java/com/meetozan/e_commerce/ui/sign_up/SignUpFragment.kt
@@ -6,19 +6,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.meetozan.e_commerce.R
+import com.meetozan.e_commerce.databinding.FragmentSignUpBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SignUpFragment : Fragment() {
 
     private val signUpViewModel : SignUpViewModel by viewModels()
+    private lateinit var binding : FragmentSignUpBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    ): View {
+        binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvBackToSignIn.setOnClickListener {
+            findNavController().navigate(R.id.action_signUpFragment_to_signInFragment)
+        }
+
+        binding.imageBackToSignIn.setOnClickListener {
+            findNavController().navigate(R.id.action_signUpFragment_to_signInFragment)
+        }
+
+
     }
 
 }
