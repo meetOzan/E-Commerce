4c5a379f0956c37479b9ff91fa98180bd351cddb
Mert Ozan Kahraman <mozankahraman69@gmail.com>
2023-04-14 21:49:31 +0300

    BottomAppBar added.

--- app/src/main/java/com/meetozan/e_commerce/MainActivity.kt
+++ app/src/main/java/com/meetozan/e_commerce/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.meetozan.e_commerce
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,6 +16,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomBar()
     }
 
+    private fun bottomBar() {
+        val bottomBar = findViewById<BottomNavigationView>(R.id.bottomNav)
+        bottomBar.background = null
+        bottomBar.menu.getItem(2).isEnabled = false
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        NavigationUI.setupWithNavController(
+            bottomBar,
+            navHostFragment.navController
+        )
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        val bottomAppBar = findViewById<BottomAppBar>(R.id.bottomAppBar)
+
+        navHostFragment.navController.addOnDestinationChangedListener { _, destination, _ ->
+            if ((destination.id == R.id.signInFragment) || (destination.id == R.id.signUpFragment)) {
+                bottomBar.visibility = View.GONE
+                bottomAppBar.visibility = View.GONE
+                fab.visibility = View.GONE
+            } else {
+                bottomBar.visibility = View.VISIBLE
+                bottomAppBar.visibility = View.VISIBLE
+                fab.visibility = View.VISIBLE
+            }
+        }
+
+    }
 }
