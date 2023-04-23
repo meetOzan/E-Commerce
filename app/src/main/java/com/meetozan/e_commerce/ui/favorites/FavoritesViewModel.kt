22957ade2e79111e2f24b6e5a54bdecae027c1d0
Mert Ozan Kahraman <mozankahraman69@gmail.com>
2023-04-23 15:31:40 +0300

    Product Repository fully implemented.

--- app/src/main/java/com/meetozan/e_commerce/ui/favorites/FavoritesViewModel.kt
+++ app/src/main/java/com/meetozan/e_commerce/ui/favorites/FavoritesViewModel.kt
@@ -1,17 +1,11 @@
 package com.meetozan.e_commerce.ui.favorites
 
-import android.content.Context
-import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.toObject
 import com.meetozan.e_commerce.data.model.model.Product
 import com.meetozan.e_commerce.data.repository.ProductRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -21,9 +15,6 @@ import kotlin.coroutines.CoroutineContext
 class FavoritesViewModel @Inject constructor(
     private val productRepository: ProductRepository,
     private val ioDispatcher: CoroutineContext,
-    private val firebaseFirestore: FirebaseFirestore,
-    private val firebaseAuth: FirebaseAuth,
-    @ApplicationContext val appContext: Context
 ) : ViewModel() {
 
     private val _favoritesList = MutableLiveData<List<Product>>()
@@ -38,25 +29,11 @@ class FavoritesViewModel @Inject constructor(
         getFavorites()
     }
 
-    private fun getFavorites() =
-        firebaseFirestore.collection("users")
-            .document(firebaseAuth.currentUser?.email.toString())
-            .collection("favorites")
-            .addSnapshotListener { querySnapshot, firestoreException ->
-                firestoreException?.let {
-                    Toast.makeText(appContext, it.message, Toast.LENGTH_LONG).show()
-                    return@addSnapshotListener
-                }
-                querySnapshot?.let {
-                    val favoritesList: ArrayList<Product> = ArrayList()
-                    for (document in it) {
-                        val favorites = document.toObject<Product>()
-                        favoritesList.add(favorites)
-                        _product.postValue(favorites)
-                        _favoritesList.postValue(favoritesList)
-                    }
-                }
-            }
+    private fun getFavorites() {
+        CoroutineScope(ioDispatcher).launch {
+            productRepository.getFavorites(_favoritesList, _product)
+        }
+    }
 
     fun addToFavorites(product: Product, hashMap: HashMap<Any, Any>) {
         CoroutineScope(ioDispatcher).launch {
